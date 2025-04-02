a2a9208c5d9ebff41a522ae55b785bacf3ef91f0
Dima <sh_diman@inbox.ru>
2025-04-02 14:15:35 +0500

    first homework

---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+import '../Message.css';
+
+function Message({text}) {
+    return (
+        <div className="message">
+            {text}
+        </div>
+    );
+}
+
+export default Message;
+
+
